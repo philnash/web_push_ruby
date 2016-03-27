c1f9a821f625db819feef49c95deb98cfcfc0e0e
Phil Nash <philnash@gmail.com>
2016-03-27 11:04:26 +0100

    Config for gcm_api_key

--- lib/web_push.rb
+++ lib/web_push.rb
@@ -1,5 +1,31 @@
 require "web_push/version"
 
+
 module WebPush
-  # Your code goes here...
+  # Yields the module itself so that it can be configured using a block
+  #
+  # @example Set the gcm_api_key to "TEST"
+  #   WebPush.configure do |config|
+  #     config.gcm_api_key = "TEST"
+  #   end
+  #
+  # @yield self
+  # @return self
+  def self.configure(&block)
+    yield self
+    self
+  end
+
+  # Attribute reader for the gcm_api_key, a Google Cloud Messaging API key.
+  #
+  # @return [String] Returns the gcm_api_key
+  def self.gcm_api_key; @gcm_api_key; end
+
+  # Set the gcm_api_key.
+  #
+  # @param [String] A Google Cloud Messaging API key
+  # @return [String] Returns the API key that was set
+  def self.gcm_api_key=(gcm_api_key)
+    @gcm_api_key = gcm_api_key
+  end
 end
--- spec/web_push_spec.rb
+++ spec/web_push_spec.rb
@@ -5,7 +5,17 @@ describe WebPush do
     expect(WebPush::VERSION).not_to be nil
   end
 
-  xit 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a gcm_api_key attribute' do
+    api_key = "TEST"
+    WebPush.gcm_api_key = api_key
+    expect(WebPush.gcm_api_key).to be(api_key)
+  end
+
+  it 'can be configured with a gcm_api_key' do
+    api_key = "TEST"
+    WebPush.configure do |config|
+      config.gcm_api_key = api_key
+    end
+    expect(WebPush.gcm_api_key).to be(api_key)
   end
 end
